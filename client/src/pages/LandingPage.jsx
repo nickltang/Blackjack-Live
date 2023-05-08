4b7bccfcb9d68cf8d7cb834031ca26ae3f57e9f3
Trong Le <t.q.le.email@gmail.com>
2023-05-08 09:43:17 -0700

    Add landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import Navigation from '../components/Navigation'
 
 /*
@@ -8,13 +11,49 @@ import Navigation from '../components/Navigation'
         - Button that links to create account page
         - Button that links to login page
 */
+const LoginPageURL = 'http://localhost:8000/login'
+const CreateAccountPageURL = 'http://localhost:8000/create-account'
+
 const LandingPage = () => {
+  // State
+  const [name, setName] = useState("{insert name}")
+
+  // Hooks
+  const navigate = useNavigate()
+
+  // Handlers
+  const handleLogin = () => {
+    console.log("Landing Page")
+    axios.post(LoginPageURL)
+      .then(res => {
+        console.log(res)
+        navigate('/Login')
+      }) 
+  }
+
+  const handleCreateAccount = () => {
+    console.log("Create Account Page")
+    axios.post(CreateAccountPageURL)
+      .then(res => {
+        console.log(res)
+        navigate('/create-account')
+      }) 
+  }
   return (
     <>
-        <Navigation />
-        <div className='mt-5 my-auto text-center'>
-          <h1>Landing Page</h1>
+      <Navigation/>
+      <div className='mt-5 text-center'>
+        <h1>Welcome to Blackjack Live</h1>
+        <p className='mt-4'>Create a game or join a game to start playing</p>
+        <div className='mt-3'>
+          <Button variant="outline-primary" onClick={handleCreateAccount}>
+            Create Account
+          </Button>
+          <Button variant="outline-success" className='mx-2' onClick={handleLogin}>
+            Login 
+          </Button>
         </div>
+      </div>
     </>
   )
 }
