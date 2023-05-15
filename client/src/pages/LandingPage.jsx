617b5ec4e2bca865b351a1bbd3a83fcc5dc4bec5
Trong Le <t.q.le.email@gmail.com>
2023-05-15 03:02:39 -0700

    add landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -33,20 +33,40 @@ const LandingPage = () => {
   }
   return (
     <>
-      <Navigation/>
+      <Navigation />
       <div className='mt-5 text-center'>
         <h1>Welcome to Blackjack Live</h1>
-        <p className='mt-4'>Create a game or join a game to start playing</p>
-        <div className='mt-3'>
+        <div className='landingContainer'>
+        <div className='landingContainer__text'>
+          <div className='ruleText'>
+            <h2> Simple rule</h2>
+            <p>The goal is to get a hand total as close to 21 as possible without going over.
+              Players are dealt two cards and can choose to receive additional cards or
+              stay with their current hand. The dealer also plays and must follow specific rules.
+              The player wins if their hand total is higher than the dealer's without exceeding 21.
+              Getting an Ace and a 10-value card initially results in a blackjack and an automatic win,
+              unless the dealer also has a blackjack.</p>
+          </div>
+          <p className='mt-4'>Create a game or join a game to start playing</p>
           <Button variant="outline-primary" onClick={handleCreateAccount}>
             Create Account
           </Button>
           <Button variant="outline-success" className='mx-2' onClick={handleLogin}>
-            Login 
-          </Button>
+            Login
+          </Button> 
+        </div>
+        <div class='landingContainer_video'>
+        <iframe width="560" height="315" 
+          src="https://www.youtube.com/embed/eyoh-Ku9TCI" 
+          title="YouTube video player" frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; 
+          encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen></iframe>
+        </div>
         </div>
       </div>
     </>
+
   )
 }
 
